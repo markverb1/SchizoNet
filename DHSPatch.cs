fc39cf79833cd3b1398dece7415c24a6be07b3e6
markverb1 <markverb4@gmail.com>
2025-12-27 12:58:50 +0200

    i don't know

--- DHSPatch.cs
+++ DHSPatch.cs
@@ -1,6 +1,13 @@
 ﻿namespace Schizo;
 
-public class DHSPatch
+using HarmonyLib;
+using Hacknet;
+
+[HarmonyPatch(typeof(DLCHubServer), nameof(DLCHubServer.DoLoadingPlayerInScreen))]
+public class NoWooshPatch
 {
-    
+    static void Prefix(DLCHubServer __instance)
+    {
+        __instance.HasStartedWoosh = true;
+    }
 }
